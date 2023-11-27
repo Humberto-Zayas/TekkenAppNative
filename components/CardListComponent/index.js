89e3f0c880d34f32656d9c6e65044a584043dc69
Humberto Zayas <soundsinhd@gmail.com>
2023-11-27 13:20:25 -0500

    move cardData to a data file

--- components/CardListComponent/index.js
+++ components/CardListComponent/index.js
@@ -1,24 +1,11 @@
 // CardListComponent.js
 import React from 'react';
 import { View, Text, FlatList, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import cardData from '../../data/cardData';
 
 const CardListComponent = ({ route, navigation }) => {
   const { character } = route.params;
   const { name, image } = character;
-
-  // Sample data for the card list
-  const cardData = [
-    { id: '1', name: 'Card 1\'s Really Awesome Long Long Title', thumbnail: require('../../data/images/cardThumbs/images.png'), rating: 4.5 },
-    { id: '2', name: 'Card 2', thumbnail: require('../../data/images/cardThumbs/images.png'), rating: 3.8 },
-    { id: '3', name: 'Card 3', thumbnail: require('../../data/images/cardThumbs/images.png'), rating: 5.0 },
-    { id: '4', name: 'Card 4', thumbnail: require('../../data/images/cardThumbs/images.png'), rating: 4.5 },
-    { id: '5', name: 'Card 5', thumbnail: require('../../data/images/cardThumbs/images.png'), rating: 3.8 },
-    { id: '6', name: 'Card 6', thumbnail: require('../../data/images/cardThumbs/images.png'), rating: 5.0 },
-    { id: '7', name: 'Card 7', thumbnail: require('../../data/images/cardThumbs/images.png'), rating: 4.5 },
-    { id: '8', name: 'Card 8', thumbnail: require('../../data/images/cardThumbs/images.png'), rating: 3.8 },
-    { id: '9', name: 'Card 9', thumbnail: require('../../data/images/cardThumbs/images.png'), rating: 5.0 },
-    // Add more cards as needed
-  ];
 
   const handleCardPress = (item) => {
     navigation.navigate('CardComponent', { item });
