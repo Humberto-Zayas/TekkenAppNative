967cf0ae7b5a9304ef98371a9bc26ecfca866c0f
Humberto Zayas <soundsinhd@gmail.com>
2023-12-10 16:36:21 -0500

    added a toggle to show login or signup on login page

--- components/Login/index.js
+++ components/Login/index.js
@@ -2,11 +2,16 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 
-const Login = ({ route }) => {
+const Login = ({ route, navigation }) => {
   const { isSignUp } = route.params || { isSignUp: false }; // Ensure default value if params is undefined
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
+
+  const handleToggle = () => {
+    // Navigate to the same screen with the opposite isSignUp value
+    navigation.setParams({ isSignUp: !isSignUp });
+  };
 
   return (
     <View style={styles.container}>
@@ -38,6 +43,13 @@ const Login = ({ route }) => {
           <Text style={styles.buttonText}>{isSignUp ? 'Sign Up' : 'Login'}</Text>
         </TouchableOpacity>
       </View>
+
+      {/* Toggle Button */}
+      <TouchableOpacity onPress={handleToggle}>
+        <Text style={styles.toggleText}>
+          {isSignUp ? 'Already have an account? Login' : "Don't have an account? Sign Up"}
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -75,6 +87,11 @@ const styles = StyleSheet.create({
   buttonText: {
     color: 'white',
     fontWeight: 'bold',
+  },
+  toggleText: {
+    marginTop: 10,
+    textAlign: 'center',
+    color: 'blue',
   },
 });
 
