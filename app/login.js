ef8d33c3e1c76dcbd6ec8662f1b3c53fbf625c2a
Humberto Zayas <soundsinhd@gmail.com>
2025-01-06 16:23:44 -0500

    Update login.js

--- app/login.js
+++ app/login.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useRouter } from 'expo-router';
 import Login from '../components/Login';
+import { useAuth } from '../utils/AuthContext';
 
 const LoginPage = () => {
+  const { user, token } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user && token) {
+      // If user exists and token is present, redirect to home
+      router.push('/');
+    }
+  }, [user, token, router]);
+
   return <Login />;
 };
 
